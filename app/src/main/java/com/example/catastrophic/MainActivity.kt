dacfa299194aff3ab0d07d9fd29e91fc1b0609d3
Zuhair-Kj <zuhairkhj@gmail.com>
2020-12-16 00:03:03 +0800

    Bring Nav component and ditch BrowseActivity

--- app/src/main/java/com/example/catastrophic/MainActivity.kt
+++ app/src/main/java/com/example/catastrophic/MainActivity.kt
@@ -2,42 +2,14 @@ package com.example.catastrophic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.lifecycle.Observer
-import com.example.browsing.BrowseCatsViewModel
-import com.example.browsing.Cat
-import com.example.browsing.databinding.ActivityBrowseCatsBinding
 import com.example.catastrophic.databinding.ActivityMainBinding
-import com.example.core.utils.NetworkHelper
-import com.example.core.utils.Resource
-import org.koin.android.ext.android.inject
-import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    private val browseViewModel: BrowseCatsViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         binding.lifecycleOwner = this
         setContentView(binding.root)
-
-        browseViewModel.catsLiveData.observe(this, Observer {
-            when(it.status) {
-                Resource.Status.SUCCESS -> {
-                    val s = it.data?.fold(StringBuilder(), { acc: StringBuilder, cat: Cat ->
-                        acc.append( "${cat.id ?: "id"} - " )
-                    }).toString()
-                    Toast.makeText(this@MainActivity, s, Toast.LENGTH_SHORT).show()
-                }
-                else -> {
-                    // Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
-                }
-            }
-        })
-
-              browseViewModel.fetchCats()
     }
 }
--- browsing/src/main/java/com/example/browsing/BrowsCatsFragment.kt
+++ browsing/src/main/java/com/example/browsing/BrowsCatsFragment.kt
@@ -1,25 +1,26 @@
 package com.example.browsing
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.browsing.databinding.ActivityBrowseCatsBinding
+import com.example.browsing.databinding.FragmentBrowseCatsBinding
 import com.example.core.utils.NetworkHelper
 import com.example.core.utils.Resource
 import com.google.android.material.snackbar.Snackbar
 import org.koin.android.ext.android.inject
-import java.lang.StringBuilder
 
-class BrowseCatsActivity: AppCompatActivity() {
-    private lateinit var binding: ActivityBrowseCatsBinding
+class BrowsCatsFragment: Fragment() {
+    lateinit var binding: FragmentBrowseCatsBinding
 
-    private val browseViewModel: BrowseCatsViewModel by viewModels()
+    private val browseViewModel: BrowseCatsViewModel by activityViewModels()
     private val catsAdapter = CatsAdapter()
     private val networkHelper: NetworkHelper by inject()
     private var tmpIsOnline: Boolean? = null
@@ -34,21 +35,22 @@ class BrowseCatsActivity: AppCompatActivity() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityBrowseCatsBinding.inflate(layoutInflater)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentBrowseCatsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = this
-        setContentView(binding.root)
+        subscribeToNetworkChanges()
         configureRecyclerView()
 
-        browseViewModel.catsLiveData.observe(this, Observer {
+        browseViewModel.catsLiveData.observe(viewLifecycleOwner, Observer {
             when(it.status) {
                 Resource.Status.SUCCESS -> {
                     binding.lottieLoader.visibility = View.GONE
                     binding.list.visibility = View.VISIBLE
-                    val s = it.data?.fold(StringBuilder(), { acc: StringBuilder, cat: Cat ->
-                        acc.append( "${cat.id ?: "id"} - " )
-                    }).toString()
                     catsAdapter.replaceItems(it.data ?: emptyList())
                 }
                 Resource.Status.LOADING -> {
@@ -64,52 +66,55 @@ class BrowseCatsActivity: AppCompatActivity() {
                     if (catsAdapter.itemCount == 0)
                         binding.offlineGroup.visibility = View.VISIBLE
                 }
+                else -> {}
             }
         })
-
-        if (savedInstanceState == null) {
-            networkHelper.connectivityLiveData.observe(this, Observer {
-                it?.let { isOnline ->
-                    if (isOnline ) {
-                        if (tmpIsOnline == false) {
-                            onlineSnackbar.show()
-                            offlineSnackbar.dismiss()
-                            catsAdapter.clear()
-                        }
-                    } else {
-                        onlineSnackbar.dismiss()
-                        offlineSnackbar.show()
-                    }
-                    browseViewModel.fetchCats()
-                }
-                tmpIsOnline = it
-            })
-        }
 
         binding.buttonRetry.setOnClickListener {
             browseViewModel.fetchCats()
         }
     }
 
+    private fun subscribeToNetworkChanges() {
+            networkHelper.connectivityLiveData.observe(viewLifecycleOwner, Observer {
+                it?.let { isOnline ->
+                    if (isOnline ) {
+                        if (tmpIsOnline == false) {
+                            onlineSnackbar.show()
+                            offlineSnackbar.dismiss()
+//                            catsAdapter.clear()
+                        }
+                    } else {
+                        onlineSnackbar.dismiss()
+                        offlineSnackbar.show()
+                    }
+                    if (catsAdapter.itemCount == 0) {
+                        browseViewModel.fetchCats()
+                    }
+                }
+                tmpIsOnline = it
+            })
+    }
+
     private fun configureRecyclerView() {
         binding.list.adapter = catsAdapter
-        binding.list.layoutManager = GridLayoutManager(this, 3)
+        binding.list.layoutManager = GridLayoutManager(requireContext(), 3)
         binding.list.addOnScrollListener(object: RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
-                    val lastItemIndex =
-                        (binding.list.layoutManager as GridLayoutManager).findLastVisibleItemPosition()
-                    if (lastItemIndex > catsAdapter.itemCount - 6
-                        && networkHelper.connectivityLiveData.value == true
-                        && browseViewModel.catsLiveData.value?.status != Resource.Status.LOADING) {
-                        browseViewModel.fetchCats()
-                        Toast.makeText(
-                            this@BrowseCatsActivity,
-                            "$lastItemIndex",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                val lastItemIndex =
+                    (binding.list.layoutManager as GridLayoutManager).findLastVisibleItemPosition()
+                if (lastItemIndex > catsAdapter.itemCount - 6
+                    && networkHelper.connectivityLiveData.value == true
+                    && browseViewModel.catsLiveData.value?.status != Resource.Status.LOADING) {
+                    browseViewModel.fetchCats()
+                    Toast.makeText(
+                        requireContext(),
+                        "$lastItemIndex",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
+            }
         })
     }
 }
